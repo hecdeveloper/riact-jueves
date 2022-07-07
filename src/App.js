07928768d4847722e2a0eb4edc8e3933f47b9ef8
Hector Ibarra <hector@oneseventech.com>
2022-07-07 15:23:04 -0400

    todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { TodoCounter } from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
 // import './App.css';
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar agua", completed: false },
+  { text: "Estudiar React", completed: false },
+];
 
 function App() {
   return (
-  <>
-  </>
+    <>
+      <TodoCounter todos={todos} />
+      <TodoSearch />
+      <TodoList>
+        {todos.map( (todo, index) => (
+          
+          <TodoItem key={index} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </>
   );
 }
 
